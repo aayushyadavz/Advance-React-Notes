f547929340657de7a7ba3170101cc280be07fa7d
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2024-10-04 15:37:08 +0530

    React Fiber Notes.

--- React-04/src/components/Body.js
+++ React-04/src/components/Body.js
@@ -11,13 +11,16 @@ const Body = () => {
               <input type="text" className="search-input" placeholder="Search Food or Restaurant" />
               <button>Search</button>
             </div>
-            <div className="filter-btn">
-                <button onClick={() => {
-                    const filteredList = listOfRestaurants.filter((restaurant) => (
-                        restaurant.info.avgRating > 4.2
-                    ))
-                    setlistOfRestaurants(filteredList)
-                }}>Top Rated Restaurants</button>
+            <div className="filter">
+                <button 
+                    className="filter-btn" 
+                    onClick={() => {
+                        const filteredList = listOfRestaurants.filter(
+                            (res) => res.info.avgRating > 4.2
+                        )
+                        setlistOfRestaurants(filteredList)
+                    }}  
+                >Top Rated Restaurants</button>
             </div>
             <div className="res-container">
                 { listOfRestaurants.map( (restaurant) => (
@@ -30,3 +33,19 @@ const Body = () => {
 }
 
 export default Body
+
+// Notes : 
+
+// How React works behind the scenes:
+
+// React uses something called a "virtual DOM," which is a simple version of the actual DOM.
+// The virtual DOM is made up of JavaScript objects that represent the structure of the real DOM.
+
+// When the UI changes (like clicking a filter button), React creates an updated virtual DOM.
+// React then compares the previous virtual DOM with the new one to find any differences, this process is called "diffing."
+
+// For example, if you have a list of cards and apply a filter, React checks what has changed between the old list and the filtered list.
+
+// React only updates the parts of the real DOM that need to change, instead of reloading the entire UI. This makes the app faster and more efficient.
+
+// React repeats this process whenever the state variable updates. React keeps an eye on the state variable, and as soon as we call setListOfRestaurants, React identifies the difference and updates the UI.
