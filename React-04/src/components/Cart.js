7196a1e6532c896357636689ce4eb5eca5c83249
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2024-11-17 14:33:49 +0530

    Made a cart page by adding items in it on clicking menu card add button.

--- React-04/src/components/Cart.js
+++ React-04/src/components/Cart.js
@@ -11,14 +11,14 @@ const Cart = () => {
     }
 
     return (
-        <div className="flex justify-center w-full pt-10 min-h-screen bg-gray-200">
+        <div className="flex justify-center pt-10 min-h-screen bg-gray-200">
             <div className="w-3/5 mb-8 shadow-xl bg-white">
             { items.length === 0 ? <h1 className="text-xl font-medium text-center mt-5">Your cart is empty, Add some food!</h1> : 
                 <div>
                     <h1 className="text-center font-bold mt-5 mb-9 text-2xl">Cart (Added items)</h1>
-                    <div className="flex justify-center mt-3 flex-col text-center px-28">
+                    <div className="text-center px-28">
                         { items.map((item) => <CartItems itemsData={item}/>)}
-                        <div className="mt-9 mb-9">
+                        <div className="mt-9 mb-4">
                             <button 
                                 className="w-3/12 bg-orange-500 py-1 rounded-lg font-semibold text-white"
                                 onClick={() => handleClick()}
--- React-04/src/components/CartItems.js
+++ React-04/src/components/CartItems.js
@@ -1,7 +1,11 @@
+import { MENU_IMG_URL } from "../utils/constants"
 
 const CartItems = ({itemsData}) => {
+    console.log(itemsData);
+    
     return (
-        <div className="flex justify-between px-3 pt-3 text-lg">
+        <div className="flex justify-between px-3 items-center text-lg border-b-[1px] border-gray-300">
+            <img className="w-14 h-12 object-cover rounded-lg my-1" src={MENU_IMG_URL + itemsData.imageId} />
             <h4 className="font-medium">{itemsData.name}</h4>
             <p className="text-gray-600">₹{itemsData.defaultPrice / 100 || itemsData.price / 100}</p>
         </div>
